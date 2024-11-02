e45771d84fde1acd3ac8e44ddda2b8ef9ee3cbbe
Alexander Braekevelt <alexander.braekevelt@gmail.com>
2024-11-02 20:58:39 +0100

    Add examples

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { Camera } from "@mediapipe/camera_utils";
 import {
   drawConnectors,
   drawLandmarks,
@@ -12,25 +11,23 @@ import {
   Results,
 } from "@mediapipe/holistic";
 
-// Constants
-const WIDTH = 1280;
-const HEIGHT = 720;
-
 // DOM elements
 const scoreElement = document.getElementById("score") as HTMLElement;
+const selectElement = document.getElementById("select") as HTMLSelectElement;
+const exampleElement = document.getElementById(
+  "show-example"
+) as HTMLInputElement;
 const tipElement = document.getElementById("show-tip") as HTMLInputElement;
 const cameraElement = document.getElementById(
   "show-camera"
 ) as HTMLInputElement;
-const videoElement = document.getElementById("input") as HTMLVideoElement;
 const canvasElement = document.getElementById("output") as HTMLCanvasElement;
 const canvasCtx = canvasElement.getContext("2d") as CanvasRenderingContext2D;
-
-// Resize to screen
-canvasElement.style.width = `${WIDTH}`;
-canvasElement.style.height = `${HEIGHT}`;
-canvasElement.width = WIDTH;
-canvasElement.height = HEIGHT;
+const videoElement1 = document.createElement("video");
+let videoElement2 = document.createElement("video");
+videoElement2.loop = true;
+videoElement2.autoplay = true;
+videoElement2.muted = false;
 
 // Movements
 const movements = [
@@ -39,50 +36,26 @@ const movements = [
 ];
 let currentMovement = 0;
 
-// Tip
-let showTip = false;
-tipElement.addEventListener("click", () => {
-  showTip = tipElement.checked;
-  currentMovement = 0;
-});
-
-// Camera
-let showCamera = false;
-cameraElement.addEventListener("click", () => {
-  showCamera = cameraElement.checked;
-});
+// Example
+const updateVideoElement2 = async () => {
+  videoElement2.src = `videos/${selectElement.value}.mp4`;
+  if (exampleElement.checked) {
+    videoElement2.load?.();
+    await videoElement2.play();
+  } else {
+    videoElement2.pause();
+  }
+};
+selectElement.addEventListener("change", updateVideoElement2);
+exampleElement.addEventListener("click", updateVideoElement2);
 
 // Score
+const SUCCESS_MESSAGE_DURATION = 3000;
+let lastScoreIncrease = Date.now() - SUCCESS_MESSAGE_DURATION;
 let score = 0;
 
 // Draw
 function draw(results: Results) {
-  // Win condition
-  const parts: Record<string, NormalizedLandmark> = {
-    leftHip: results.poseLandmarks?.[POSE_LANDMARKS.LEFT_HIP],
-    leftIndexFinger: results.leftHandLandmarks?.[8],
-    rightShoulder: results.poseLandmarks?.[POSE_LANDMARKS.RIGHT_SHOULDER],
-  };
-
-  const movement = movements[currentMovement];
-  const part = parts[movement[0]];
-  const target = parts[movement[1]];
-
-  if (part && target) {
-    if (
-      Math.abs(part.x - target.x) < 0.1 &&
-      Math.abs(part.y - target.y) < 0.1
-    ) {
-      currentMovement += 1;
-    }
-  }
-
-  if (currentMovement >= movement.length) {
-    score += 1;
-    scoreElement.innerHTML = `${score}`;
-    currentMovement = 0;
-  }
-
   // Draw
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
@@ -102,7 +75,7 @@ function draw(results: Results) {
   canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
 
   // Only overwrite missing pixels.
-  if (showCamera) {
+  if (cameraElement.checked) {
     canvasCtx.globalCompositeOperation = "destination-atop";
     if (results.image) {
       canvasCtx.drawImage(
@@ -167,25 +140,65 @@ function draw(results: Results) {
     // });
   }
 
-  if (showTip) {
-    if (part) {
-      drawLandmarks(canvasCtx, [part], {
-        color: "#FF0000",
-        lineWidth: 5,
-      });
-    }
-    if (target) {
-      drawLandmarks(canvasCtx, [target], {
-        color: "#00FF00",
-        lineWidth: 5,
-      });
-    }
+  // Win condition
+  if (exampleElement.checked) {
+    currentMovement = 0;
+  } else {
+    const parts: Record<string, NormalizedLandmark> = {
+      leftHip: results.poseLandmarks?.[POSE_LANDMARKS.LEFT_HIP],
+      leftIndexFinger: results.leftHandLandmarks?.[8],
+      rightShoulder: results.poseLandmarks?.[POSE_LANDMARKS.RIGHT_SHOULDER],
+    };
+
+    const movement = movements[currentMovement];
+    const part = parts[movement[0]];
+    const target = parts[movement[1]];
+
     if (part && target) {
-      drawConnectors(canvasCtx, [part, target], [[0, 1]], {
-        color: "#00FF00",
-        lineWidth: 3,
-      });
+      if (
+        Math.abs(part.x - target.x) < 0.1 &&
+        Math.abs(part.y - target.y) < 0.1
+      ) {
+        currentMovement += 1;
+      }
     }
+
+    if (currentMovement >= movement.length) {
+      score += 1;
+      scoreElement.innerHTML = `${score}`;
+      currentMovement = 0;
+      lastScoreIncrease = Date.now();
+    }
+
+    if (tipElement.checked) {
+      if (part) {
+        drawLandmarks(canvasCtx, [part], {
+          color: "#FF0000",
+          lineWidth: 5,
+        });
+      }
+      if (target) {
+        drawLandmarks(canvasCtx, [target], {
+          color: "#00FF00",
+          lineWidth: 5,
+        });
+      }
+      if (part && target) {
+        drawConnectors(canvasCtx, [part, target], [[0, 1]], {
+          color: "#00FF00",
+          lineWidth: 3,
+        });
+      }
+    }
+  }
+  if (Date.now() - lastScoreIncrease < SUCCESS_MESSAGE_DURATION) {
+    canvasCtx.font = "64px serif";
+    canvasCtx.textAlign = "center";
+    canvasCtx.fillText(
+      "Goed gedaan!",
+      canvasElement.width / 2,
+      canvasElement.height / 2
+    );
   }
 
   canvasCtx.restore();
@@ -212,10 +225,49 @@ holistic.setOptions({
 });
 holistic.onResults(draw);
 
-// Camera
-const camera = new Camera(videoElement, {
-  onFrame: async () => holistic.send({ image: videoElement }),
-  width: WIDTH,
-  height: HEIGHT,
-});
-camera.start();
+// Rendering loop
+async function render() {
+  const videoElement = !exampleElement.checked ? videoElement1 : videoElement2;
+
+  if (videoElement.readyState > 1) {
+    // Fit to frame
+    const canvasElementWidth =
+      ((videoElement.videoWidth || window.innerWidth) * window.innerHeight) /
+      (videoElement.videoHeight || window.innerHeight);
+    if (canvasElement.width !== canvasElementWidth) {
+      canvasElement.width = canvasElementWidth;
+    }
+    if (window.innerHeight !== canvasElement.height) {
+      canvasElement.height = window.innerHeight;
+    }
+
+    // Detect and draw
+    await holistic.send({ image: videoElement });
+  }
+
+  // Rerender on next frame
+  window.requestAnimationFrame(render);
+}
+
+// Start rendering loop
+const isWebCamSupported = !!navigator.mediaDevices?.getUserMedia;
+if (!isWebCamSupported) {
+  alert(
+    "Je browser is niet ondersteund. Probeer in een andere browser of een ander toestel."
+  );
+  location.reload();
+} else {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(async (stream) => {
+      videoElement1.srcObject = stream;
+      videoElement1.addEventListener("loadeddata", render);
+      videoElement1.load?.();
+      await videoElement1.play();
+    })
+    .catch((e) => {
+      console.log(e?.message);
+      alert("Er is een fout opgetreden. Probeer opnieuw.");
+      location.reload();
+    });
+}
